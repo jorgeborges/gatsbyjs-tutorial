9afc4bfd60d6b8906e66aeb28dc871698d0d18fb
Jorge A. Borges <contact@jorgeborges.me>
2017-12-27 09:37:36 +1100

    Adding page title to blog posts

--- tutorial-part-four/src/templates/blog-post.js
+++ tutorial-part-four/src/templates/blog-post.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 export default ({ data }) => {
   const post = data.markdownRemark;
   return (
     <div>
-      <h1>{post.frontmatter.title}</h1>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{ data.site.siteMetadata.title } - { post.frontmatter.title }</title>
+      </Helmet>
+      <h1>{ post.frontmatter.title }</h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
     </div>
   );
@@ -15,6 +20,11 @@ export const query = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       frontmatter {
+        title
+      }
+    }
+    site {
+      siteMetadata {
         title
       }
     }
